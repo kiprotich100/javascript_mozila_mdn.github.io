36582c2aa092be0fa94ba8324e2ad497f67babdf
kiprotich100 <timcheruiyot18@gmail.com>
2024-09-03 07:04:42 +0300

    archived old code and started main js course

--- scripts/archive.js
+++ scripts/archive.js
@@ -31,4 +31,44 @@ document.querySelector("html").addEventListener("click", function () {
 // other ways of calling anonymous functions
 document.querySelector("html").addEventListener("click", () => {
     alert("Ouch! Stop Poking me!")
-})
+})  
+ // mdn intro to js mdn tutorial
+ 
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png"){
+        myImage.setAttribute("src", "images/learn_js.jpeg");
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+};
+
+// Adding a personalized welcome page 
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUsername() {
+    const myName = prompt("Please enter your name.");//prompt displays a dialog box
+    if (!myName) {
+        setUsername();
+    } else {
+    localStorage.setItem("name", myName);
+    //localStorage is an API which give storage
+    //setItem creates and and store a data item as name, setting its value to the myName 
+    myHeading.textContent = `Mozila is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUsername();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUsername()
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,8 @@
-const myImage = document.querySelector("img");
+const button = document.querySelector("button");
 
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "images/firefox-icon.png"){
-        myImage.setAttribute("src", "images/learn_js.jpeg");
-    } else {
-        myImage.setAttribute("src", "images/firefox-icon.png");
-    }
-};
+button.addEventListener("click", updateName);
 
-// Adding a personalized welcome page 
-
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1");
-
-function setUsername() {
-    const myName = prompt("Please enter your name.");//prompt displays a dialog box
-    if (!myName) {
-        setUsername();
-    } else {
-    localStorage.setItem("name", myName);
-    //localStorage is an API which give storage
-    //setItem creates and and store a data item as name, setting its value to the myName 
-    myHeading.textContent = `Mozila is cool, ${myName}`;
-    }
-}
-
-if (!localStorage.getItem("name")) {
-    setUsername();
-} else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `Mozilla is cool, ${storedName}`;
-}
-
-myButton.onclick = () => {
-    setUsername()
+function updateName() {
+    const name = prompt("Enter a new name");
+    button.textContent = `Player 1:${name}`
 }
